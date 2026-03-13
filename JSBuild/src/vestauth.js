645ca3f430df5c14cbfdc7de136c54a48b8cdb14
Miles J. Litteral <Mandaloe2@gmail.com>
2026-03-13 00:11:35 -0400

    Update vestauth.js

--- JSBuild/src/vestauth.js
+++ JSBuild/src/vestauth.js
@@ -1,6 +1,32 @@
 // vestauth.js
 import fetch from "node-fetch";
 
+/*
+  Quickstart
+  Install vestauth and initialize your agent:
+
+  npm i -g vestauth
+  vestauth agent init
+
+  Your agent sets secrets with a simple curl endpoint:
+
+  vestauth agent curl -X POST https://as2.dotenvx.com/set -d '{"KEY":"value"}'
+  And your agent gets secrets with a simple curl endpoint:
+
+  vestauth agent curl "https://as2.dotenvx.com/get?key=KEY"
+  That is it. This primitive unlocks secret access for agents without human-in-the-loop, oauth flows, or API keys.
+
+  in js you can just do:   
+  //Set  
+  generatedtKey = SHA256.generate(...)
+  await VestAuthClient.set(generatedtKey);
+
+  //Get
+  if (config.secretKey) {
+      providerToken = await VestAuthClient.get(config.secretKey);
+  }
+*/
+
 export class VestAuthClient {
   constructor(endpoint = "https://as2.dotenvx.com") {
     this.endpoint = endpoint;
